5326803bf537df53ad9ef302dbb6f0212b7e3f41
Ivaylo1992 <ivailop123@gmail.com>
2025-08-05 06:24:29 +0300

    feat: process stock report

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-# This is a sample Python script.
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def process_stock_report(
+        input_file_path: str,
+        output_file_path: str,
+        concept_filter: str = "OUTLET",
+        columns_to_drop: list = None,
+        columns_to_format_as_int: list = None,
+        exel_header_row: int = 2,
+    ) -> None:
+        """
+        Processes an available stock report, filters by concept,
+        drops specified columns, formats columns to integer, and saves to a new Excel file.
+
+        Args:
+        input_file_path (str): Path to the input Excel file.
+        output_file_path (str): Path to save the processed Excel file.
+        concept_filter (str): The value in the 'Concept' column to filter by. Defaults to 'OUTLET'.
+        columns_to_drop (list): A list of column names to drop. Defaults to original list.
+        columns_to_format_as_int (list): A list of column names to convert to integer type.
+                                          Defaults to ['AVAILABLE', 'RESERVED'].
+        excel_header_row (int): The 0-indexed row number to use as the header. Defaults to 2.
+        """
+
+        if columns_to_drop is None:
+          columns_to_drop = [
+              'STOCK_UPDATE',
+              'SIZE',
+              'Subcategory',
+              'Licence',
+              'Barcode',
+              'STOCK_WITHOUT_REZERVED',
+              'REZERVED'
+          ]
+
+        if columns_to_format_as_int is None:
+          columns_to_format_as_int = ['AVAILABLE']
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        # Dynamically create the dictionary for .assign()
+        format_assignments = {
+          col: lambda x, c=col: x[c].astype(int) for col in columns_to_format_as_int
+        }
 
+        df_processed = (
+          pd.read_excel(input_file_path, header=exel_header_row)
+          .drop(columns=columns_to_drop)
+          .query(f'Concept == "{concept_filter}"')
+          .reset_index(drop=True)
+          .assign(**format_assignments)
+        )
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        df_processed.to_excel(output_file_path, index=False)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        print(f"Saved to {output_file_path}")
+
+process_stock_report(
+    input_file_path='data/Available Stock Report.xlsx',
+    output_file_path='data/new_report_basic.xlsx'
+)
